f0098080677b3f0aef58600b0e208b6d353b2b2e
Dario MF <ferrada_86@hotmail.com>
2021-03-23 05:46:08 +0100

    update model user

--- models/usuario.js
+++ models/usuario.js
@@ -1,44 +1,43 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const UsuarioSchema = new Schema({
-    name: { 
-        type: String, 
-        required: [true, 'nombre is required'] 
+    name: {
+        type: String,
+        required: [true, 'nombre is required']
     },
-    email: { 
-        type: String, 
+    email: {
+        type: String,
         required: [true, 'email is required'],
         unique: true
     },
-    password: { 
-        type: String, 
-        required: [true, 'password is required'] 
+    password: {
+        type: String,
+        required: [true, 'password is required']
     },
-    img:{ 
-        type: String 
+    img: {
+        type: String
     },
-    rol: { 
-        type: String, 
-        required: [true, 'rol is required'], 
-        default:'USER_ROLE',
-        emun: ['ADMIN_ROLE', 'USER_ROLE'] 
+    rol: {
+        type: String,
+        default: 'USER_ROLE',
+        emun: ['ADMIN_ROLE', 'USER_ROLE', 'VENTAS_ROLE']
     },
-    state: { 
-        type: Boolean, 
-        default: true 
+    state: {
+        type: Boolean,
+        default: true
     },
-    google: { 
-        type: Boolean, 
-        default: false 
+    google: {
+        type: Boolean,
+        default: false
     }
 });
 
 
-UsuarioSchema.methods.toJSON = function() {
-    const { __v, password, _id, ...usuario} = this.toObject();
+UsuarioSchema.methods.toJSON = function () {
+    const { __v, password, _id, ...usuario } = this.toObject();
     usuario.uid = _id;
     return usuario;
 };
 
 
-module.exports = model( 'Usuario', UsuarioSchema );
+module.exports = model('Usuario', UsuarioSchema);
